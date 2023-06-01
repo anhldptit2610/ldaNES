fb9ed26f458a5210959281d163c35edb77d561d6
anhldptit.2610 <anhldptit.2610@gmail.com>
2023-06-01 22:31:34 +0700

    fix some code.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include "../inc/ldaNES_6502.h"
+#include "../inc/common.h"
+#include "../inc/cpu.h"
 #include "../inc/bus.h"
 #include "../inc/cartridge.h"
+#include "../inc/ppu.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -14,30 +16,38 @@
 int main(void)
 {
 	struct cpu cpu;
-	struct cartridge cart;
+	struct ppu ppu;
+	struct bus bus = {
+		.cpu = &cpu,
+		.ppu = &ppu,
+	};
 	int clockCycle = 0;
 	FILE *filp = NULL;
 	struct cartridge nestest;
 
-	resetCPU(&cpu, &clockCycle);
-	writeUsingBus(&cpu, 0xfffc, 0x00);
-	writeUsingBus(&cpu, 0xfffd, 0xC0);
+	resetCPU(&bus, &clockCycle);
+	writeUsingBus(&bus, 0xfffc, 0x00);
+	writeUsingBus(&bus, 0xfffd, 0xC0);
 	cpu.reg.pc = 0xC000;
-	parseCartridge(&nestest, &cpu, "../src/nestest.nes");
+	parseCartridge(&nestest, &bus, "../src/test/nestest.nes");
 
-	filp = fopen("../src/log.txt", "w");
+	filp = fopen("../src/test/log.txt", "w");
 	if (!filp) {
 		fprintf(stderr, "ldaNES: can't open log file!\n");
 		exit(1);
 	}
-	writeUsingBus(&cpu, 0x0002, 0xff);
+	writeUsingBus(&bus, 0x0002, 0xff);
 	int i = 1;
 	while (1) {
-		if (i == 8992)
-			return 0;
-		fetchAndExecuteInstruction(&cpu, &clockCycle);
-		exportLog(&cpu, filp);
-		i++;
+		if (!clockCycle) {
+			if (i == 8992)
+				return 0;
+			fetchAndExecuteInstruction(&bus, &clockCycle);
+			exportLog(&bus, filp);
+			i++;
+		} else {
+			clockCycle--;
+		}
 	}
 	
 	fclose(filp);
